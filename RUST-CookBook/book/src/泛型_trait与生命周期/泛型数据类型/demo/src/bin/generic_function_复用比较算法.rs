37bf3c8033de2803e2dfdbcb2e359a761558579c
heyiyang <heyiyang@scenemap.cn>
2026-06-16 21:34:53 +0800

    feat: rust

--- RUST-CookBook/book/src/泛型_trait与生命周期/泛型数据类型/demo/src/bin/generic_function_复用比较算法.rs
+++ RUST-CookBook/book/src/泛型_trait与生命周期/泛型数据类型/demo/src/bin/generic_function_复用比较算法.rs
@@ -1,14 +1,28 @@
-fn largest<T: PartialOrd>(values: &[T]) -> &T {
-    let mut largest = &values[0];
-    for value in &values[1..] {
-        if value > largest {
-            largest = value;
+fn bubble_sort<T>(values: &mut [T])
+where
+    T: PartialOrd,
+{
+    for unsorted_end in (1..values.len()).rev() {
+        for index in 0..unsorted_end {
+            if values[index] > values[index + 1] {
+                values.swap(index, index + 1);
+            }
         }
     }
-    largest
 }
 
 fn main() {
-    assert_eq!(*largest(&[1, 3, 2]), 3);
-    assert_eq!(*largest(&['a', 'z', 'b']), 'z');
+    let mut scores = [85, 60, 100, 90];
+    bubble_sort(&mut scores);
+    assert_eq!(scores, [60, 85, 90, 100]);
+
+    let mut letters = ['z', 'a', 'c', 'b'];
+    bubble_sort(&mut letters);
+    assert_eq!(letters, ['a', 'b', 'c', 'z']);
+
+    let mut fruits = ["pear", "apple", "orange"];
+    bubble_sort(&mut fruits);
+    assert_eq!(fruits, ["apple", "orange", "pear"]);
+
+    assert_eq!(f64::NAN.partial_cmp(&1.0), None);
 }
